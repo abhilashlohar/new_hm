0bc1b87931486012b74c6382b97e6fca4c7a68d5
Abhilash Lohar <abhilashlohar01@gmail.com>
2016-06-15 18:25:17 +0530

    dsfdsf

--- app/webroot/backup_db.php
+++ app/webroot/backup_db.php
@@ -1,6 +1,6 @@
 <?php
 $output1 = shell_exec('/usr/bin/mongodump --db new_version 2>&1');
-$output1 = shell_exec('/usr/bin/zip -r /var/www/html/Housingmatters/app/webroot/dump.zip /var/www/html/Housingmatters/app/webroot/dump 2>&1');
+$output1 = shell_exec('/usr/bin/zip -r dump.zip /var/www/html/Housingmatters/app/webroot/dump 2>&1');
 header('Location: http://app.housingmatters.co.in/Hms/auto_backup_data');
 
 ?>

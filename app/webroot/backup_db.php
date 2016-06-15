a0a5a007042e63474c8ee3e4b15fd203200cc537
Abhilash Lohar <abhilashlohar01@gmail.com>
2016-06-15 18:08:59 +0530

    sdfds

--- app/webroot/backup_db.php
+++ app/webroot/backup_db.php
@@ -1,5 +1,5 @@
 <?php
 $output1 = shell_exec('/usr/bin/mongodump --db new_version 2>&1');
-$output1 = shell_exec('/usr/bin/zip -r /var/www/html/Housingmatters/app/webroot/dump.zip /var/www/html/Housingmatters/app/webrootdump 2>&1');
+$output1 = shell_exec('/usr/bin/zip -r /var/www/html/Housingmatters/app/webroot/dump.zip /var/www/html/Housingmatters/app/webroot/dump 2>&1');
 echo "Back up completed";
 ?>

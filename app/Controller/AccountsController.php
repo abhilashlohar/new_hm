8b08163f525162d97436898c591b8b7a4d9424a0
Abhilash Lohar <abhilashlohar01@gmail.com>
2016-07-16 14:45:26 +0530

    dsfdsf

--- app/Controller/AccountsController.php
+++ app/Controller/AccountsController.php
@@ -3895,8 +3895,9 @@ function trial_balance_ajax_show_sub_ledger($from=null,$to=null,$wise=null)
                 $flat_id=$data2["flat"]["flat_id"];
                 $ledger_sub_account_id = $this->requestAction(array('controller' => 'Fns', 'action' => 'ledger_sub_account_id_via_wing_id_and_flat_id_for_trail_balance'),array('pass'=>array($wing_id,$flat_id)));
                 if(sizeof($ledger_sub_account_id)>0){
-					foreach($ledger_sub_account_id as $ledger_sub_accountid)
-                        $members_for_billing[]=$ledger_sub_accountid;
+					foreach($ledger_sub_account_id as $ledger_sub_accountid){
+						 $members_for_billing[]=$ledger_sub_accountid;
+					}
                 }
                 
             }

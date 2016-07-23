0e549cdf2f087c19dddf7731dc4cc3cef936873f
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2016-07-23 16:29:18 +0530

    bill_edit

--- app/Controller/FnsController.php
+++ app/Controller/FnsController.php
@@ -741,8 +741,9 @@ function calculate_arrears_and_without_interest_edit($ledger_sub_account_id,$sta
 		$arrear_interest=0;
 	
 		$this->loadmodel('ledger');
+		$order=array('transaction_date'=>'ASC');
 		$conditions=array("ledger_account_id" =>34,"ledger_sub_account_id" => (int)$ledger_sub_account_id,'transaction_date'=>array('$lt'=>strtotime($start_date)));
-		$ledgers = $this->ledger->find('all',array('conditions'=>$conditions));
+		$ledgers = $this->ledger->find('all',array('conditions'=>$conditions,"order"=>$order));
 		
 		foreach($ledgers as $data2){
 			$debit=$data2["ledger"]["debit"];
@@ -782,6 +783,11 @@ function calculate_arrears_and_without_interest_edit($ledger_sub_account_id,$sta
 				}
 			}
 			
+			echo "<br/>";
+	echo $maint_arrear."-";
+	echo $non_maint_arrear."-";
+	echo $arrear_interest."-";
+	echo "<br/>"; 
 		}
 		$arrear_principle=$maint_arrear+$non_maint_arrear;
 		return array("maint_arrear" =>$maint_arrear,"non_maint_arrear" =>$non_maint_arrear,"arrear_principle" =>$arrear_principle,"arrear_intrest" =>$arrear_interest);

3cb9196eae81ec11468e295db75ba7ebe5752fc8
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2017-03-22 12:28:36 +0530

    broadcast

--- app/Controller/BroadcastsController.php
+++ app/Controller/BroadcastsController.php
@@ -142,8 +142,10 @@ $sms_allow=(int)$r_sms->sms_allow;
 			}
 		
 		
-		$payload = file_get_contents('http://alerts.sinfini.com/api/web2sms.php?workingkey='.$working_key.'&sender='.$sms_sender.'&to='.$mobile_im.'&message='.$massage_str.'&time='.$s_date_ex0.$s_date_ex1.$s_date_ex2.$time_h.$time_m.'&format=json');
+		$payload = file_get_contents('http://alerts.sinfini.com/api/web2sms.php?workingkey='.$working_key.'&sender='.$sms_sender.'&to='.$mobile_im.'&message='.$massage_str.'&time='.$s_date_ex0.$s_date_ex1.$s_date_ex2.$time_h.$time_m);
 		}
+		$g[]=$payload;
+		pr($g);
 	pr($payload['GID']); echo"<br/>";
 	pr($payload->GID); echo"<br/>";
 	pr($payload->Message->GID); echo"<br/>";

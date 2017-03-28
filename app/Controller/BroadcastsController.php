806ae370296eb70df48965eb6748ffe9519caa2f
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2017-03-28 16:15:03 +0530

    cancel api

--- app/Controller/BroadcastsController.php
+++ app/Controller/BroadcastsController.php
@@ -48,7 +48,8 @@ function sms_schedule_cancel($g_id=null){
 		$r_sms=$this->requestAction(array('controller' => 'Fns', 'action' => 'hms_sms_ip')); 
 		$working_key=$r_sms->working_key;
 		$sms_sender=$r_sms->sms_sender; 
-		$ge=file_get_contents('http://api-alerts.solutionsinfini.com/v4/?method=sms.schedule&api_key='.$working_key.'&groupid='.$g_id); 	
+		$ge=file_get_contents('http://api-alerts.solutionsinfini.com/v4/?method=sms.schedule&api_key='.$working_key.'&groupid='.$g_id); 
+pr($ge);		
 	exit;
 	$this->response->header('Location', 'message_view');
 	

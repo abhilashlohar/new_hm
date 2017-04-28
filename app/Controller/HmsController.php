5be7fd48d90fef6ba94422d363289f93ef7c5882
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2017-04-28 17:50:20 +0530

    hms

--- app/Controller/HmsController.php
+++ app/Controller/HmsController.php
@@ -28053,8 +28053,11 @@ $s_role_id = (int)$this->Session->read('hm_role_id');
 $s_society_id = (int)$this->Session->read('hm_society_id');
 $s_user_id = (int)$this->Session->read('hm_user_id');
 
+//$conditions =array( '$or' => array(array("user_id" => $user_id,"society_id"=>$s_society_id),array("user_id" => $user_id)));
+
 $this->loadmodel('user');
 $conditions=array("user_id" => $user_id,"society_id"=>$s_society_id);
+
 return $this->user->find('all',array('conditions'=>$conditions));
 }
 ////////////////////////////////////// End user Fetch(Accounts)///////////////////////
